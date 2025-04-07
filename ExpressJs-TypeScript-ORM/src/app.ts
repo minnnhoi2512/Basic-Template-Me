80ea06d2dd33d9313cd3c1d4ee195b26867521df
cyno-hoinm <hoinm.cyno@gmail.com>
2025-04-07 22:40:06 +0700

    cors-flex

--- ExpressJs-TypeScript-ORM/src/app.ts
+++ ExpressJs-TypeScript-ORM/src/app.ts
@@ -1,15 +1,17 @@
 // Base libraries
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
 // Base routes
 import userRoute from "./routes/user.route";
 import { connectDB, disconnectDB } from "./database/connect";
 // Swagger
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./swagger";
+// Config
+import { corsConfig } from "./types/Cors.type";
 
 dotenv.config();
 
@@ -18,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // Base config
 app.use(morgan("tiny"));
-app.use(cors());
+app.use(cors(corsConfig));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
---
+++ ExpressJs-TypeScript-ORM/src/types/Cors.type.ts
@@ -0,0 +1,17 @@
+type CorsOptions = {
+  origin: string | string[];
+  methods: string[];
+  allowedHeaders: string[];
+  exposedHeaders: string[];
+  credentials: boolean;
+  maxAge: number; // Seconds
+};
+
+export const corsConfig: CorsOptions = {
+  origin: process.env.CORS_ORIGINS?.split(",") || ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  exposedHeaders: ["Content-Range", "X-Content-Range"],
+  credentials: true,
+  maxAge: 3600,
+};
